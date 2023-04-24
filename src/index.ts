e5e7256d7b707fb38779b9eddac8e940a539afee
Serhii Vashchenko <vashenko49@gmail.com>
2023-04-24 19:34:55 +0300

    done

---
+++ src/index.ts
@@ -0,0 +1,71 @@
+import puppeteer from 'puppeteer-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+
+puppeteer.use(StealthPlugin());
+
+function isJSON(jsonString: string): boolean {
+  try {
+    const json = JSON.parse(jsonString);
+    if (json && typeof json === 'object') {
+      return true;
+    }
+  } catch (e) {
+    console.error('Error parsing JSON:', e);
+  }
+  return false;
+}
+
+const parse = async (url = 'https://dev.amidstyle.com'): Promise<string> => {
+  const browser = await puppeteer.launch({
+    headless: 'new',
+  });
+
+  try {
+    const page = await browser.newPage();
+
+    await page.goto(url);
+
+    const selector = '#data';
+
+    await page.waitForFunction(
+      (selector) => {
+        const element = document.querySelector(selector);
+
+        if (!element) return false;
+
+        return element.innerHTML.trim().length;
+      },
+      {},
+      selector,
+    );
+
+    // Get the element
+    const data = await page.$(selector);
+
+    if (!data) throw new Error('Not found element');
+
+    const textContent = await page.evaluate((el) => el.textContent, data);
+
+    if (!textContent) throw new Error('Not found textContent');
+
+    const obj = JSON.parse(textContent);
+
+    const sign = obj.sign;
+
+    if (!sign) throw new Error('Not found sign');
+
+    const newObj = {
+      sign,
+    };
+
+    return JSON.stringify(newObj);
+  } catch (error) {
+    await browser.close();
+
+    throw new Error(error);
+  }
+};
+
+parse().then((res) => {
+  console.log(res);
+});
